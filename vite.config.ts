4a074d77da2fb73dddf7512e9f6685655c14568a
Roman <4elove4ek40@gmail.com>
2023-09-29 15:23:35 +0600

    fix: font

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import vue from "@vitejs/plugin-vue";
 import path from "node:path";
 import { defineConfig } from "vite";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({ script: { defineModel: true } })],
+  plugins: [vue({ script: { defineModel: true } }), webfontDownload()],
   resolve: {
     alias: {
       "@tf-app": path.resolve(__dirname, "./src"),

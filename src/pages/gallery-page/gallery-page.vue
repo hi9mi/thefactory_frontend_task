bd2dbbff70166fe1233460044a4a67e6e5c84bab
Roman <4elove4ek40@gmail.com>
2023-10-15 11:52:40 +0600

    setupt eslint and stylelint

--- src/pages/gallery-page/gallery-page.vue
+++ src/pages/gallery-page/gallery-page.vue
@@ -1,23 +1,21 @@
 <script setup lang="ts">
-import { storeToRefs } from "pinia";
-import { computed } from "vue";
+import { storeToRefs } from 'pinia'
+import { computed } from 'vue'
 
-import { useGalleryStore } from "@tf-app/entities/gallery";
-import TfGallerySearch from "@tf-app/entities/gallery/ui/tf-gallery-search.vue";
-import TfAffix from "@tf-app/shared/ui/tf-affix.vue";
-import TfLoader from "@tf-app/shared/ui/tf-loader.vue";
-import TfPhotoCard from "@tf-app/widgets/tf-photo-card.vue";
+import { useGalleryStore } from '@tf-app/entities/gallery'
+import TfGallerySearch from '@tf-app/entities/gallery/ui/tf-gallery-search.vue'
+import TfAffix from '@tf-app/shared/ui/tf-affix.vue'
+import TfLoader from '@tf-app/shared/ui/tf-loader.vue'
+import TfPhotoCard from '@tf-app/widgets/tf-photo-card.vue'
 
-const galleryStore = useGalleryStore();
-const { randomPhotos, isLoadingGallery, photos, searchQuery } = storeToRefs(galleryStore);
+const galleryStore = useGalleryStore()
+const { randomPhotos, isLoadingGallery, photos, searchQuery } = storeToRefs(galleryStore)
 
-const isEmptySearch = computed(() => searchQuery.value?.trim().length > 0 && photos.value.length < 1);
+const isEmptySearch = computed(() => searchQuery.value?.trim().length > 0 && photos.value.length < 1)
 </script>
 
 <template>
-  <tf-gallery-search
-    v-model="searchQuery"
-  />
+  <TfGallerySearch v-model="searchQuery" />
   <main class="container">
     <div
       v-if="!isLoadingGallery"
@@ -26,12 +24,12 @@ const isEmptySearch = computed(() => searchQuery.value?.trim().length > 0 && pho
       <template v-if="searchQuery?.trim().length">
         <p
           v-if="isEmptySearch"
-          class="gallery_empty"
+          class="gallery-empty"
         >
           Ничего не найдено
         </p>
         <template v-else>
-          <tf-photo-card
+          <TfPhotoCard
             v-for="photo of photos"
             :key="photo.id"
             :photo="photo"
@@ -39,43 +37,42 @@ const isEmptySearch = computed(() => searchQuery.value?.trim().length > 0 && pho
         </template>
       </template>
       <template v-else>
-        <tf-photo-card
+        <TfPhotoCard
           v-for="photo of randomPhotos"
           :key="photo.id"
           :photo="photo"
         />
       </template>
     </div>
-    <tf-loader v-else />
-    <tf-affix />
+    <TfLoader v-else />
+    <TfAffix />
   </main>
 </template>
 
 <style scoped>
 .gallery {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-template-rows: repeat(3, 1fr);
-    grid-column-gap: 20px;
-    grid-row-gap: 20px;
-    margin: 100px 0;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  grid-gap: 20px;
+  margin: 100px 0;
 }
 
-.gallery_empty{
-    font-size: 18px;
-    text-align: center;
-    margin: 20px 0;
+.gallery-empty {
+  font-size: 18px;
+  text-align: center;
+  margin: 20px 0;
 }
 
-@media screen and (max-width: 760px) {
-    .gallery {
-        grid-template-columns: repeat(2, 1fr);
-    }
+@media screen and (width <= 760px) {
+  .gallery {
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 
-@media screen and (max-width: 560px) {
-    .gallery {
-        grid-template-columns: repeat(1, 1fr);
-    }
+@media screen and (width <= 560px) {
+  .gallery {
+    grid-template-columns: repeat(1, 1fr);
+  }
 }
 </style>

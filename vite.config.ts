9954c8d6498e4fc549b3319af199b98fe38f8ec2
Roman <4elove4ek40@gmail.com>
2023-10-21 10:00:47 +0600

    fix runtime caching max entries

--- vite.config.ts
+++ vite.config.ts
@@ -73,7 +73,7 @@ export default defineConfig(({ mode }) => {
                 },
                 expiration: {
                   maxAgeSeconds: 60 * 30,
-                  maxEntries: 18,
+                  maxEntries: 36,
                 },
               },
             },

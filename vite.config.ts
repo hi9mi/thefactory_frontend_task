550b9ac3770c34dd4415204958a4938ec34ddc00
Roman <4elove4ek40@gmail.com>
2023-10-29 12:36:21 +0600

    fix pwa runtime caching

--- vite.config.ts
+++ vite.config.ts
@@ -65,7 +65,7 @@ export default defineConfig(({ mode }) => {
           runtimeCaching: [
             {
               urlPattern: /^https:\/\/images\.unsplash\.com\/.*/i,
-              handler: 'NetworkFirst',
+              handler: 'CacheFirst',
               options: {
                 cacheName: 'api-photos-cache',
                 cacheableResponse: {
@@ -79,7 +79,7 @@ export default defineConfig(({ mode }) => {
             },
             {
               urlPattern: /^https:\/\/api\.unsplash\.com\/.*/i,
-              handler: 'NetworkFirst',
+              handler: 'CacheFirst',
               options: {
                 cacheName: 'api-cache',
                 cacheableResponse: {

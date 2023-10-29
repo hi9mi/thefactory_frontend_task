f7f678739a0f06b0227b65cabfdcc7ff626999b7
Roman <4elove4ek40@gmail.com>
2023-10-29 14:27:03 +0600

    fix bug when photos showing without search query

--- src/pages/gallery-page/index.ts
+++ src/pages/gallery-page/index.ts
@@ -1,4 +1,5 @@
 import type { RouteRecordRaw } from 'vue-router'
+import { storeToRefs } from 'pinia'
 
 import { useGalleryStore } from '@tf-app/entities/gallery'
 import { routes } from '@tf-app/routing'
@@ -13,10 +14,14 @@ const GalleryPageRoute: RouteRecordRaw = {
   },
   beforeEnter: (from) => {
     const galleryStore = useGalleryStore()
+    const { photos } = storeToRefs(galleryStore)
     const searchQuery = from.query.q?.toString().trim()
     const page = Number(from.query.p)
+
     if (searchQuery && (searchQuery.length > 0))
       galleryStore.getPhotos(searchQuery, page)
+    else if (photos.value)
+      photos.value = null
 
     galleryStore.getRandomPhotos()
 

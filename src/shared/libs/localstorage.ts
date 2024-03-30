3771c0562559226d4aefec50dbe5374b8dc2f2f4
Roman <4elove4ek40@gmail.com>
2024-03-30 11:00:15 +0500

    favorite photos

--- src/shared/libs/localstorage.ts
+++ src/shared/libs/localstorage.ts
@@ -1,6 +1,3 @@
-import type { MaybeRef } from 'vue'
-import { ref, toValue, watchEffect } from 'vue'
-
 export function getItemFromLS<T = unknown>(key: string, defaultValue: T): T {
   const dataFromLS = globalThis.localStorage.getItem(key)
 
@@ -9,26 +6,4 @@ export function getItemFromLS<T = unknown>(key: string, defaultValue: T): T {
 
 export function setItemToLS<T = unknown>(key: string, value: T) {
   globalThis.localStorage.setItem(key, JSON.stringify(value))
-}
-
-export function usePersistLS<
-  T = any,
->(source: MaybeRef<T>, key: string, sync?: boolean) {
-  const data = ref(getItemFromLS(key, source))
-
-  if (sync && window) {
-    window.addEventListener('storage', (event) => {
-      if (event.storageArea !== globalThis.localStorage)
-        return
-
-      if (event.newValue)
-        data.value = JSON.parse(event.newValue)
-    })
-  }
-
-  watchEffect(() => {
-    setItemToLS(key, toValue(data))
-  })
-
-  return data
 }

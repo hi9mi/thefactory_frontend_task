7c37b9310cf1df4da8f2869b8aefbbf6acb0615a
Roman <4elove4ek40@gmail.com>
2024-04-02 16:17:50 +0500

    fix: usePaginationData

--- src/shared/libs/vue/use-pagination-data.ts
+++ src/shared/libs/vue/use-pagination-data.ts
@@ -6,22 +6,16 @@ interface Options {
   currentPage?: MaybeRef<number>
 }
 
-export function usePagination<T>(data: MaybeRef<T[]>, options: Options = {}) {
+export function usePaginationData<T>(data: MaybeRef<T[]>, options: Options = {}) {
   const { limit = 9, currentPage: page = 1 } = options
 
   const currentPage = ref(page)
   const totalPages = computed(() => Math.ceil(toRef(data).value.length / limit))
   const slicedData = computed(() => toRef(data).value.slice((currentPage.value - 1) * limit, (currentPage.value - 1) * limit + limit))
 
-  const onNextPage = () => {
-    if (currentPage.value < totalPages.value)
-      currentPage.value += 1
+  function changePage(newPage: number) {
+    currentPage.value = newPage
   }
 
-  const onPreviousPage = () => {
-    if (currentPage.value > 0)
-      currentPage.value -= 1
-  }
-
-  return { data: slicedData, currentPage, totalPages, onNextPage, onPreviousPage }
+  return { data: slicedData, currentPage, totalPages, changePage }
 }

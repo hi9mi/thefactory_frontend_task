0e0bb5e161bee51f2a56aa9575dde3f6ef115dbc
Roman <4elove4ek40@gmail.com>
2024-05-06 12:13:49 +0500

    test: storage/local

---
+++ src/shared/libs/storage/local.spec.ts
@@ -0,0 +1,36 @@
+import { describe, expect, it } from 'vitest'
+
+import { getItemFromLS, setItemToLS } from './local'
+
+describe('getItemFromLS', () => {
+  it('should return default value for non-existent key', () => {
+    const key = 'test_key'
+    const defaultValue = 'default'
+
+    expect(getItemFromLS(key, defaultValue)).toBe(defaultValue)
+  })
+
+  it('should return parsed JSON data for existing key', () => {
+    const key = 'test_key'
+    const value = { data: 'value' }
+
+    localStorage.setItem(key, JSON.stringify(value))
+
+    expect(getItemFromLS(key, null)).toEqual(value)
+
+    localStorage.clear()
+  })
+})
+
+describe('setItemToLS', () => {
+  it('should set the value in localStorage as JSON string', () => {
+    const key = 'test_key'
+    const value = { data: 'value' }
+
+    setItemToLS(key, value)
+
+    expect(localStorage.getItem(key)).toBe(JSON.stringify(value))
+
+    localStorage.clear()
+  })
+})

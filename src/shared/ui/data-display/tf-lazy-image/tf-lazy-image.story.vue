d0b71a1091a306e2c506585cde81af36ef231461
Roman <4elove4ek40@gmail.com>
2024-05-17 13:12:42 +0500

    fix(ui/data-display): tf-lazy-image story

--- src/shared/ui/data-display/tf-lazy-image/tf-lazy-image.story.vue
+++ src/shared/ui/data-display/tf-lazy-image/tf-lazy-image.story.vue
@@ -2,48 +2,36 @@
 import { reactive } from 'vue'
 import { logEvent } from 'histoire/client'
 
-import type { Photo } from '@tf-app/shared/api'
-import TfLoader from '@tf-app/shared/ui/feedback/tf-loader/tf-loader.vue'
-
 import TfLazyImage from './tf-lazy-image.vue'
 
-const state = reactive<{
-  photo: Photo | null
-  loading: boolean
-}>({
-  photo: null,
-  loading: false,
+const state = reactive({
+  key: 'initial',
 })
-
-async function loadRandomPhoto() {
-  state.loading = true
-  const randomPhotosURL = new URL('photos/random', 'https://api.unsplash.com')
-  randomPhotosURL.searchParams.set('client_id', '_KrzP7Yj9ObontNacjoQht00agosJQygX9l-ctvlLbE')
-
-  const response = await fetch(randomPhotosURL)
-  state.photo = await response.json()
-  state.loading = false
+function rerenderImage() {
+  state.key = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2)
 }
-
-loadRandomPhoto()
 </script>
 
 <template>
   <Story>
     <Variant title="TfLazyImage">
-      <TfLoader v-if="state.loading || !state.photo" />
       <TfLazyImage
-        v-else
-        :original-src="`${state.photo.urls.raw}&w=640&h=640&dpr=2&q=80`"
-        :alt="state.photo.alt_description"
-        :placeholder-src="state.photo.urls.thumb"
+        :key="state.key"
+        :original-src="`https://images.unsplash.com/photo-1713350472357-704e65809226?ixid=M3w0MDgwODh8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTU5Mjk2MjV8&ixlib=rb-4.0.3&w=640&h=640&dpr=2&q=80&crop=entropy&random=${state.key}`"
+        alt="a very long tunnel with some lights in it"
+        placeholder-src="https://images.unsplash.com/photo-1713350472357-704e65809226?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDgwODh8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTU5Mjk2MjV8&ixlib=rb-4.0.3&q=80&w=200"
         @loaded="logEvent('loaded', $event)"
         @intersected="logEvent('intersected', $event)"
         @error="logEvent('error', $event)"
       />
+
       <template #controls>
-        <HstButton @click="loadRandomPhoto">
-          Load image
+        <HstButton
+          color="primary"
+          class="htw-p-2"
+          @click="rerenderImage"
+        >
+          Rerender image
         </HstButton>
       </template>
     </Variant>

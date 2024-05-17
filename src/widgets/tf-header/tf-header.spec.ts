4327709ef80f2f15273a118fe572e689834522af
Roman <4elove4ek40@gmail.com>
2024-05-17 17:17:49 +0500

    test(widgets/tf-header): added basic tests

---
+++ src/widgets/tf-header/tf-header.spec.ts
@@ -0,0 +1,52 @@
+import { reactive } from 'vue'
+import { useRoute } from 'vue-router'
+import { mount } from '@vue/test-utils'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+
+import TfHeader from './tf-header.vue'
+
+import SearchIcon from '~icons/tf-icons/search'
+
+vi.mock('vue-router', () => ({
+  useRoute: vi.fn(),
+}))
+describe('tf-header widget', () => {
+  let wrapper: ReturnType<typeof mount<typeof TfHeader>>
+
+  beforeEach(() => {
+    const RouterLinkStub = {
+      template: '<div><slot></slot></div>',
+    }
+
+    // @ts-expect-error mock
+    useRoute.mockReturnValue(reactive({
+      path: '/',
+    }))
+    wrapper = mount(TfHeader, {
+      global: {
+        stubs: {
+          RouterLink: RouterLinkStub,
+        },
+      },
+    })
+  })
+
+  afterEach(() => {
+    vi.clearAllMocks()
+    wrapper.unmount()
+    // @ts-expect-error cleanup wrapper
+    wrapper = undefined
+  })
+
+  it('should shows search icon when not on search route', () => {
+    expect(wrapper.findComponent(SearchIcon).exists()).toBe(true)
+  })
+
+  it('should hides search icon when on search route', async () => {
+    useRoute().path = '/search'
+
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.findComponent(SearchIcon).exists()).toBe(false)
+  })
+})

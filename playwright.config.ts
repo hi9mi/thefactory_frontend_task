dcecf8167150334edaff76cbf02f32475656a287
Roman <4elove4ek40@gmail.com>
2024-05-30 15:39:17 +0500

    chore(playwright): update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,14 +28,16 @@ export default defineConfig({
     baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    video: 'on-first-retry',
+    headless: true,
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'], headless: true },
+      use: { ...devices['Desktop Chrome'] },
     },
 
     // {

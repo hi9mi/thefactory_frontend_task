fcd8ff48ff71be11b17e0c0f985af7095296502a
Roman <4elove4ek40@gmail.com>
2025-08-19 13:22:45 +0500

    fix: remove unnecessary files

--- src/shared/libs/storage/local.ts
+++
@@ -1,9 +0,0 @@
-export function getItemFromLS<T = unknown>(key: string, defaultValue: T): T {
-  const dataFromLS = globalThis.localStorage.getItem(key)
-
-  return dataFromLS ? JSON.parse(dataFromLS) : defaultValue
-}
-
-export function setItemToLS<T = unknown>(key: string, value: T) {
-  globalThis.localStorage.setItem(key, JSON.stringify(value))
-}
